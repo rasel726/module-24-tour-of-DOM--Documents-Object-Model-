a9b1ed99f3089fcfe657c4778ab4c6cb6f01ab02
rasel726 <md.rasel2726@gmail.com>
2022-08-16 00:36:46 +0600

    Create HTML elements using Javascript and appendChild

---
+++ js/append.js
@@ -0,0 +1,48 @@
+// 1. Where to add
+const placesList = document.getElementById('places-list');
+// 2. what to be added
+const li = document.createElement('li');
+li.innerText = 'pahartoli bon';
+
+// 3. add the child
+placesList.appendChild(li);
+
+// 1. where to add
+const mainContainer = document.getElementById('main-content');
+
+// 2. what to be added
+const section = document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText = 'My Food List';
+section.appendChild(h1);
+
+const ul = document.createElement('ul');
+const li1 = document.createElement('li')
+li1.innerText = 'biriyani';
+ul.appendChild(li1);
+
+const li2 = document.createElement('li')
+li2.innerText = 'borhani';
+ul.appendChild(li2);
+
+const li3 = document.createElement('li')
+li3.innerText = 'salat';
+ul.appendChild(li3);
+
+section.appendChild(ul);
+
+mainContainer.appendChild(section);
+
+// set innerHTML directly
+const sectionDress = document.createElement('section');
+sectionDress.innerHTML = `
+<h1>My Dress Section</h1>
+<ul>
+    <li>T-Shirt</li>
+    <li>Lungi</li>
+    <li>Sando Genji</li>
+</ul>
+`
+
+mainContainer.appendChild(sectionDress);
+
